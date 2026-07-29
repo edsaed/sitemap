0fd7be0278190b15bcf506b4485db77c8fae9fbc
MUHMMAD SA'ID AL-FATAH <50813172+edsaed@users.noreply.github.com>
2026-07-29 13:23:00 +0700

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,2 +1,136 @@
-/* jsdelivr url (https://cdn.jsdelivr.net/gh/edsaed/sitemap@main/sitemap.js) by https://www.edsaed.eu.org */
-var postTitle=new Array();var postUrl=new Array();var postMp3=new Array();var postDate=new Array();var postLabels=new Array();var postBaru=new Array();var sortBy="titleasc";var tocLoaded=false;var numChars=250;var postFilter="";var numberfeed=0;function loadtoc(a){function b(){if("entry" in a.feed){var d=a.feed.entry.length;numberfeed=d;ii=0;for(var h=0;h<d;h++){var n=a.feed.entry[h];var e=n.title.$t;var m=n.published.$t.substring(0,10);var j;for(var g=0;g<n.link.length;g++){if(n.link[g].rel=="alternate"){j=n.link[g].href;break}}var o="";for(var g=0;g<n.link.length;g++){if(n.link[g].rel=="enclosure"){o=n.link[g].href;break}}var c="";if("category" in n){for(var g=0;g<n.category.length;g++){c=n.category[g].term;var f=c.lastIndexOf(";");if(f!=-1){c=c.substring(0,f)}postLabels[ii]=c;postTitle[ii]=e;postDate[ii]=m;postUrl[ii]=j;postMp3[ii]=o;if(h<10){postBaru[ii]=true}else{postBaru[ii]=false}ii=ii+1}}}}}b();sortBy="titleasc";sortPosts(sortBy);sortlabel();tocLoaded=true;displayToc2();document.write('</br><a href="https://www.edsaed.eu.org" style="font-size: 10px; text-decoration:none; color: #616469;">Sitemap By edsaed.eu.org</a>')}function filterPosts(a){scroll(0,0);postFilter=a;displayToc(postFilter)}function allPosts(){sortlabel();postFilter="";displayToc(postFilter)}function sortPosts(d){function c(e,g){var f=postTitle[e];postTitle[e]=postTitle[g];postTitle[g]=f;var f=postDate[e];postDate[e]=postDate[g];postDate[g]=f;var f=postUrl[e];postUrl[e]=postUrl[g];postUrl[g]=f;var f=postLabels[e];postLabels[e]=postLabels[g];postLabels[g]=f;var f=postMp3[e];postMp3[e]=postMp3[g];postMp3[g]=f;var f=postBaru[e];postBaru[e]=postBaru[g];postBaru[g]=f}for(var b=0;b<postTitle.length-1;b++){for(var a=b+1;a<postTitle.length;a++){if(d=="titleasc"){if(postTitle[b]>postTitle[a]){c(b,a)}}if(d=="titledesc"){if(postTitle[b]<postTitle[a]){c(b,a)}}if(d=="dateoldest"){if(postDate[b]>postDate[a]){c(b,a)}}if(d=="datenewest"){if(postDate[b]<postDate[a]){c(b,a)}}if(d=="orderlabel"){if(postLabels[b]>postLabels[a]){c(b,a)}}}}}function sortlabel(){sortBy="orderlabel";sortPosts(sortBy);var a=0;var b=0;while(b<postTitle.length){temp1=postLabels[b];firsti=a;do{a=a+1}while(postLabels[a]==temp1);b=a;sortPosts2(firsti,a);if(b>postTitle.length){break}}}function sortPosts2(d,c){function e(f,h){var g=postTitle[f];postTitle[f]=postTitle[h];postTitle[h]=g;var g=postDate[f];postDate[f]=postDate[h];postDate[h]=g;var g=postUrl[f];postUrl[f]=postUrl[h];postUrl[h]=g;var g=postLabels[f];postLabels[f]=postLabels[h];postLabels[h]=g;var g=postMp3[f];postMp3[f]=postMp3[h];postMp3[h]=g;var g=postBaru[f];postBaru[f]=postBaru[h];postBaru[h]=g}for(var b=d;b<c-1;b++){for(var a=b+1;a<c;a++){if(postTitle[b]>postTitle[a]){e(b,a)}}}}function displayToc(a){var l=0;var h="";var e="Judul Artikel";var m="Klik untuk sortir berdasarkan judul";var d="Tanggal";var k="Klik untuk Sortir bedasarkan tanggal";var c="Kategori";var j="";if(sortBy=="titleasc"){m+=" (descending)";k+=" (newest first)"}if(sortBy=="titledesc"){m+=" (ascending)";k+=" (newest first)"}if(sortBy=="dateoldest"){m+=" (ascending)";k+=" (newest first)"}if(sortBy=="datenewest"){m+=" (ascending)";k+=" (oldest first)"}if(postFilter!=""){j="Klik untuk menampilkan semua"}h+="<table>";h+="<tr>";h+='<td class="toc-header-col1">';h+='<a href="javascript:toggleTitleSort();" title="'+m+'">'+e+"</a>";h+="</td>";h+='<td class="toc-header-col2">';h+='<a href="javascript:toggleDateSort();" title="'+k+'">'+d+"</a>";h+="</td>";h+='<td class="toc-header-col3">';h+='<a href="javascript:allPosts();" title="'+j+'">'+c+"</a>";h+="</td>";h+='<td class="toc-header-col4">';h+="Download MP3";h+="</td>";h+="</tr>";for(var g=0;g<postTitle.length;g++){if(a==""){h+='<tr><td class="toc-entry-col1"><a href="'+postUrl[g]+'">'+postTitle[g]+'</a></td><td class="toc-entry-col2">'+postDate[g]+'</td><td class="toc-entry-col3">'+postLabels[g]+'</td><td class="toc-entry-col4"><a href="'+postMp3[g]+'">Download</a></td></tr>';l++}else{z=postLabels[g].lastIndexOf(a);if(z!=-1){h+='<tr><td class="toc-entry-col1"><a href="'+postUrl[g]+'">'+postTitle[g]+'</a></td><td class="toc-entry-col2">'+postDate[g]+'</td><td class="toc-entry-col3">'+postLabels[g]+'</td><td class="toc-entry-col4"><a href="'+postMp3[g]+'">Download</a></td></tr>';l++}}}h+="</table>";if(l==postTitle.length){var f='<span class="toc-note">Menampilkan Semua '+postTitle.length+" Artikel<br/></span>"}else{var f='<span class="toc-note">Menampilkan '+l+" artikel dengan kategori '";f+=postFilter+"' dari "+postTitle.length+" Total Artikel<br/></span>"}var b=document.getElementById("toc");b.innerHTML=f+h}function displayToc2(){var a=0;var b=0;while(b<postTitle.length){temp1=postLabels[b];document.write("<p/>");document.write('<p><a href="/search/label/'+temp1+'">'+temp1+"</a></p><ol>");firsti=a;do{document.write("<li>");document.write('<a href="'+postUrl[a]+'">'+postTitle[a]+"</a>");if(postBaru[a]==true){document.write(' - <strong><em><span style="color: rgb(255, 0, 0);">New !!</span> </em></strong>')}document.write("</li>");a=a+1}while(postLabels[a]==temp1);b=a;document.write("</ol>");sortPosts2(firsti,a);if(b>postTitle.length){break}}}function toggleTitleSort(){if(sortBy=="titleasc"){sortBy="titledesc"}else{sortBy="titleasc"}sortPosts(sortBy);displayToc(postFilter)}function toggleDateSort(){if(sortBy=="datenewest"){sortBy="dateoldest"}else{sortBy="datenewest"}sortPosts(sortBy);displayToc(postFilter)}function showToc(){if(tocLoaded){displayToc(postFilter);var a=document.getElementById("toclink")}else{alert("Just wait... TOC is loading")}}function hideToc(){var a=document.getElementById("toc");a.innerHTML="";var b=document.getElementById("toclink");b.innerHTML='<a href="#" onclick="scroll(0,0); showToc(); Effect.toggle(\'toc-result\',\'blind\');">?? Menampilkan Daftar Isi</a> <img src="http://radiorodja.googlepages.com/new_1.gif"/>'}function looptemp2(){for(var a=0;a<numberfeed;a++){document.write("<br>");document.write('Post Link    : <a href="'+postUrl[a]+'">'+postTitle[a]+"</a><br>");document.write('Download mp3  : <a href="'+postMp3[a]+'">'+postTitle[a]+"</a><br>");document.write("<br>")}};
+/**
+ * Universal & Professional Blogger HTML Sitemap / Table of Contents
+ * Repository: https://github.com/edsaed/sitemap
+ * Features: Auto-pagination (>500 posts), Search filter, Category grouping, Clean DOM
+ */
+
+(function () {
+  'use strict';
+
+  let allEntries = [];
+  let categories = {};
+
+  // 1. Render Tampilan Utama
+  function renderSitemapUI() {
+    const container = document.getElementById('sitemap-container');
+    if (!container) return;
+
+    let html = `
+      <div class="sitemap-controls">
+        <input type="text" id="sitemap-search" placeholder="Cari judul artikel..." onkeyup="window.filterSitemapPosts()" />
+        <span class="sitemap-stats">Total: <strong id="sitemap-total-count">${allEntries.length}</strong> Artikel</span>
+      </div>
+      <div id="sitemap-content"></div>
+    `;
+
+    container.innerHTML = html;
+    buildCategoryData(allEntries);
+    displaySitemap();
+  }
+
+  // 2. Olah Data Kategori & Sorting
+  function buildCategoryData(entries) {
+    categories = {};
+
+    entries.forEach((entry) => {
+      const title = entry.title.$t;
+      const published = entry.published.$t.substring(0, 10);
+
+      let postUrl = '';
+      if (entry.link) {
+        const altLink = entry.link.find((l) => l.rel === 'alternate');
+        if (altLink) postUrl = altLink.href;
+      }
+
+      const labels = entry.category
+        ? entry.category.map((c) => c.term)
+        : ['Lainnya'];
+
+      labels.forEach((label) => {
+        if (!categories[label]) categories[label] = [];
+        categories[label].push({ title, url: postUrl, date: published });
+      });
+    });
+  }
+
+  // 3. Tampilkan List Artikel
+  function displaySitemap(filterText = '') {
+    const contentBox = document.getElementById('sitemap-content');
+    if (!contentBox) return;
+
+    let html = '';
+    const sortedCategories = Object.keys(categories).sort();
+    let totalVisible = 0;
+
+    sortedCategories.forEach((category) => {
+      const filteredPosts = categories[category].filter((post) =>
+        post.title.toLowerCase().includes(filterText.toLowerCase())
+      );
+
+      if (filteredPosts.length > 0) {
+        totalVisible += filteredPosts.length;
+        html += `<div class="sitemap-category-box">`;
+        html += `<h3 class="sitemap-cat-title"><a href="/search/label/${encodeURIComponent(category)}" target="_blank" rel="noopener">${category}</a> <span>(${filteredPosts.length})</span></h3>`;
+        html += `<ol class="sitemap-list">`;
+
+        // Sort artikel A-Z
+        filteredPosts.sort((a, b) => a.title.localeCompare(b.title));
+
+        filteredPosts.forEach((post) => {
+          html += `<li>
+            <a href="${post.url}" target="_blank" rel="noopener">${post.title}</a>
+            <span class="sitemap-date">${post.date}</span>
+          </li>`;
+        });
+
+        html += `</ol></div>`;
+      }
+    });
+
+    if (totalVisible === 0) {
+      html = `<div class="sitemap-empty">Artikel tidak ditemukan.</div>`;
+    }
+
+    contentBox.innerHTML = html;
+  }
+
+  // 4. Fitur Filter / Pencarian Live
+  window.filterSitemapPosts = function () {
+    const searchInput = document.getElementById('sitemap-search');
+    const query = searchInput ? searchInput.value : '';
+    displaySitemap(query);
+  };
+
+  // 5. Recursive Fetcher (Paging Otomatis untuk >500 Artikel)
+  window.fetchBloggerFeed = function (data) {
+    const entries = data.feed.entry || [];
+    allEntries = allEntries.concat(entries);
+
+    const totalResults = parseInt(data.feed.openSearch$totalResults.$t, 10);
+    const startIndex = parseInt(data.feed.openSearch$startIndex.$t, 10);
+    const itemsPerPage = parseInt(data.feed.openSearch$itemsPerPage.$t, 10);
+
+    // Render secara bertahap saat data masuk
+    renderSitemapUI();
+
+    // Jika masih ada sisa artikel, ambil halaman berikutnya secara otomatis
+    if (startIndex + itemsPerPage <= totalResults) {
+      const nextIndex = startIndex + itemsPerPage;
+      loadFeedScript(nextIndex);
+    }
+  };
+
+  function loadFeedScript(startIndex = 1) {
+    const currentDomain = window.location.origin;
+    const script = document.createElement('script');
+    script.src = `${currentDomain}/feeds/posts/summary?alt=json-in-script&start-index=${startIndex}&max-results=500&callback=fetchBloggerFeed`;
+    document.body.appendChild(script);
+  }
+
+  // Inisialisasi saat DOM SIAP
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => loadFeedScript(1));
+  } else {
+    loadFeedScript(1);
+  }
+})();
